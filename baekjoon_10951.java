9868425ee85642da3d04a92f1c8341651cdae089
Heegene <64513810+Heegene@users.noreply.github.com>
2020-06-09 16:04:48 +0900

    Baekjoon solution(JAVA)
    .

--- baekjoon_10951.java
+++ baekjoon_10951.java
@@ -1,25 +1,32 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class baekjoon_10951 {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		while(sc.hasNextInt()) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			System.out.println(a + b);	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String input = "";
+
+		while ((input = br.readLine()) != null && input.length() != 0) { // 입력값 없이 엔터쳤을때 종료
+			String[] cutoff = input.split(" ");
+			
+			int A = Integer.parseInt(cutoff[0]);
+			int B = Integer.parseInt(cutoff[1]);
+			
+			String r = A + B + "\n";
+			
+			bw.write(r, 0, r.length());
 		}
 		
-		
-		
-		
-		
-		
+
+		bw.flush();
+		bw.close();
 	}
+
 }
-
-
-
